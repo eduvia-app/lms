cf5cc19fd5bdef0b8ec393341c18b3a3c1c2bd80
stanig2106 <stanig2106@gmail.com>
2025-08-13 22:33:12 +0200

    i18n setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import frappeui from 'frappe-ui/vite'
@@ -25,7 +25,7 @@ export default defineConfig({
 		}),
 	],
 	server: {
-		allowedHosts: ['fs', 'per2'],
+		allowedHosts: ['lms.localhost', 'fs', 'per2'],
 	},
 	resolve: {
 		alias: {
